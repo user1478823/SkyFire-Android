de1a973d7a85956de6299d3fb214b8c2f502e3e5
Danilo <danilot.89@gmail.com>
2017-11-30 11:40:18 +0100

    commit

--- skyfire/src/main/java/com/example/skyfire/MyFirebaseInstanceIdService.java
+++ skyfire/src/main/java/com/example/skyfire/MyFirebaseInstanceIdService.java
@@ -2,19 +2,18 @@ package com.example.skyfire;
 
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+
 
 /**
  * Created by ttlnisoffice on 11/29/17.
  */
 
-public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
-
+public class MyFirebaseInstanceIdService /*extends FirebaseInstanceIdService*/ {
+/*
     @Override
     public void onTokenRefresh() {
         super.onTokenRefresh();
         Log.d("TOKEN ==>>",
                 FirebaseInstanceId.getInstance().getToken());
-    }
+    }*/
 }
